2109780f8d3901b5cdcf449169c179b57b2811f7
ajit-1995 <113002483+ajit-1995@users.noreply.github.com>
2023-04-17 14:05:13 +0530

    my second commit

--- src/components/layout/sidebar/Sidebar.jsx
+++ src/components/layout/sidebar/Sidebar.jsx
@@ -1,7 +1,17 @@
 
 import "./sidebar.scss";
 import DashboardIcon from '@mui/icons-material/Dashboard';
-
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import StoreIcon from '@mui/icons-material/Store';
+import CreditCardIcon from '@mui/icons-material/CreditCard';
+import LocalShippingOutlinedIcon from '@mui/icons-material/LocalShippingOutlined';
+import InsertChartOutlinedIcon from '@mui/icons-material/InsertChartOutlined';
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import SettingsSystemDaydreamOutlinedIcon from '@mui/icons-material/SettingsSystemDaydreamOutlined';
+import PsychologyOutlinedIcon from '@mui/icons-material/PsychologyOutlined';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import ExitToAppOutlinedIcon from '@mui/icons-material/ExitToAppOutlined';
 
 const Sidebar = () => {
   return (
@@ -12,17 +22,65 @@ const Sidebar = () => {
         <hr />
         <div className='center'>
             <ul>
+                <p className="title">MAIN</p>
                 <li>
-                    <DashboardIcon />
+                    <DashboardIcon className="icon" />
                     <span>Dashboard</span>
                 </li>
-                <li><span>Dashboard</span></li>
-                <li><span>Dashboard</span></li>
-                <li><span>Dashboard</span></li>
-                <li><span>Dashboard</span></li>
+                <p className="title">LIST</p>
+                <li>
+                    <PersonOutlineOutlinedIcon className="icon" />
+                    <span>Users</span>
+                </li>
+                <li>
+                    <StoreIcon className="icon"/>
+                    <span>products</span>
+                </li>
+                <li>
+                    <CreditCardIcon className="icon"/>
+                    <span>Orders</span>
+                </li>
+                <li>
+                    <LocalShippingOutlinedIcon className="icon"/>
+                    <span>Delivery</span>
+                </li>
+                <p className="title">USEFUL</p>
+                <li>
+                    <InsertChartOutlinedIcon className="icon"/>
+                    <span>Stats</span>
+                </li>
+                <li>
+                    <NotificationsActiveIcon className="icon" />
+                    <span>Notifications</span>
+                </li>
+                <li>
+                    <SettingsSystemDaydreamOutlinedIcon className="icon"/>
+                    <span>System Heath</span>
+                </li>
+                <p className="title">SERVICE</p>
+                <li>
+                    <PsychologyOutlinedIcon className="icon"/>
+                    <span>Logs</span>
+                </li>
+                <p className="title">USER</p>
+                <li>
+                    <SettingsOutlinedIcon className="icon"/>
+                    <span>Settings</span>
+                </li>
+                <li>
+                    <AccountCircleOutlinedIcon className="icon" />
+                    <span>Profile</span>
+                </li>
+                <li>
+                    <ExitToAppOutlinedIcon className="icon"/>
+                    <span>Logout</span>
+                </li>
             </ul>
         </div>
-        <div className='bottom'>color option</div>
+        <div className='bottom'>
+            <div className="colorOption"></div>
+            <div className="colorOption"></div>
+        </div>
     </div>
   )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import './home.scss';
 import Sidebar from "../../components/layout/sidebar/Sidebar";
+import Navbar from "../../components/layout/navbar/Navbar";
+import Widget from "../../components/layout/widget/Widget";
 
 const Home = () => {
     return(
         <div className='home'>
             <Sidebar/>
-            <div className="homeContainer">container</div>
+            <div className="homeContainer">
+                <Navbar />
+                <div className="widgets">
+                    <Widget type="user" />
+                    <Widget type="order" />
+                    <Widget type="earning" />
+                    <Widget type="balance" />
+                </div>
+            </div>
         </div>
     )
 }
